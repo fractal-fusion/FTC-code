47fe3deb3a794c586569d6f2561369c1d8353bbf
fractal-fusion <fractal.fusion27188@gmail.com>
2024-09-28 10:17:14 -0400

    fixed tank drive for two motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tankteleopsinglejoystick.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tankteleopsinglejoystick.java
@@ -13,17 +13,14 @@ public class tankteleopsinglejoystick extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        DcMotor leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        DcMotor rightMotor = hardwareMap.dcMotor.get("rightMotor");
 
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
@@ -33,15 +30,11 @@ public class tankteleopsinglejoystick extends LinearOpMode {
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x; // Counteract imperfect strafing
 
-            double frontLeftPower = (y + x);
-            double backLeftPower = (y + x);
-            double frontRightPower = (y - x);
-            double backRightPower = (y - x);
+            double leftPower = (y + x);
+            double rightPower = (y - x);
 
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
+            leftMotor.setPower(leftPower);
+            rightMotor.setPower(rightPower);
         }
     }
 }
