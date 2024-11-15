1d6fc270f2305c29baf8ad0ee3cd9a9603c040bd
fractal-fusion <fractal.fusion@27188@gmail.com>
2024-11-15 15:31:26 -0500

    updated debug wrist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debugWrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debugWrist.java
@@ -9,15 +9,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class debugWrist extends LinearOpMode {
 
     public Servo wrist = null;
+    public Servo claw = null;
     double servoposition = 0.5;
     public void runOpMode() {
         wrist  = hardwareMap.get(Servo.class, "wrist");
-
+        claw = hardwareMap.get(Servo.class, "claw");
         waitForStart();
 
         while (opModeIsActive()) {
             if (gamepad1.x) {
                 wrist.setPosition(0);
+                claw.setPosition(0);
             }
             if (gamepad1.right_bumper) {
                 servoposition += 0.1;
@@ -27,6 +29,7 @@ public class debugWrist extends LinearOpMode {
             }
 
             wrist.setPosition(servoposition);
+            claw.setPosition(servoposition);
 
             telemetry.addData("wrist position: ", wrist.getPosition());
             telemetry.update();
