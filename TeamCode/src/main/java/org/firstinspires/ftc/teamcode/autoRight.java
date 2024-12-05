74efb1c14f435433f811acf3b3421443b371aa1f
kkkwwww-ui <129519318+kkkwwww-ui@users.noreply.github.com>
2024-12-04 20:32:29 -0500

    Revert "changed auto name and also initialized the roadrunner library"
    This reverts commit 3b91739914df9e8cfd3cbad9e271bea52cdb7c51.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoRight.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="Left Auto", group="Robot")
+@Autonomous(name="Right Auto", group="Robot")
 public class autoRight extends LinearOpMode {
 
     @Override
