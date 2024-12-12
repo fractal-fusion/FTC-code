87f8d2372f088028937bbb927de0a72ef1e7e80f
fractal-fusion <fractal.fusion@27188@gmail.com>
2024-12-12 17:19:00 -0500

    basic odometry auto implemented

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,42 +14,42 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                // Set bot constraints maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-tilelength, -65, Math.toRadians(90)))
-                //TODO: close claw to grab sample
+                // close claw to grab sample
                 //move to bucket
                 .splineTo(new Vector2d(-52.5,-52.5), Math.toRadians(225))
-                //TODO: move arm, extend viperslide and open claw to drop sample, then retract viperslides
+                // move arm, extend viperslide and open claw to drop sample, then retract viperslides
                 // and move back arm to collection position
                 //turn to pick up first rightmost sample from drivers perspective
                 .turnTo(Math.toRadians(90))
                 //move to rightmost sample
                 .strafeTo(new Vector2d(-48.2,-36.5))
-                //TODO: close claw to grab sample
+                // close claw to grab sample
                 //move to bucket
                 .splineTo(new Vector2d(-52.5,-52.5), Math.toRadians(210))
-                //TODO: move arm, extend viperslide and open claw to drop sample, then retract viperslides
+                // move arm, extend viperslide and open claw to drop sample, then retract viperslides
                 // and move back arm to collection position
                 //turn to pick up the middle sample
                 .turnTo(Math.toRadians(90))
                 //move to middle sample
                 .strafeTo(new Vector2d(-(2*tilelength+11),-37.1))
-                //TODO: close claw to grab sample
+                // close claw to grab sample
                 //move to bucket
                 .splineTo(new Vector2d(-52.5,-52.5), Math.toRadians(245))
-                //TODO: move arm, extend viperslide and open claw to drop sample, then retract viperslides
+                // move arm, extend viperslide and open claw to drop sample, then retract viperslides
                 // and move back arm to collection position
                 //turn to pickup leftmost sample
                 .turnTo(Math.toRadians(180))
                 //move to leftmost sample
                 .strafeTo(new Vector2d(-(2*tilelength+11),-27.5))
-                //TODO: close claw to grab sample
+                // close claw to grab sample
                 //move to basket
                 .splineTo(new Vector2d(-52.5,-52.5), Math.toRadians(150))
-                //TODO: move arm, extend viperslide and open claw to drop sample, then retract viperslides
+                // move arm, extend viperslide and open claw to drop sample, then retract viperslides
                 // and move back arm to collection position
 
                 //park
