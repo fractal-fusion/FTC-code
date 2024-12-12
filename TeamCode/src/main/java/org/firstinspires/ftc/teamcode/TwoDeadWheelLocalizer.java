4ffc5c24c2dd5a38d55374b107ea79efc0e67839
fractal-fusion <fractal.fusion@27188@gmail.com>
2024-12-12 16:06:10 -0500

    moved out from the roadrunner package because its unusable inside of it, and changed the orientation of the imu to be correct (hopefully)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoDeadWheelLocalizer.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.roadrunner;
+package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.DualNum;
@@ -20,7 +20,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 import org.firstinspires.ftc.robotcore.external.navigation.UnnormalizedAngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-import org.firstinspires.ftc.teamcode.roadrunner.messages.TwoDeadWheelInputsMessage;
+import org.firstinspires.ftc.teamcode.messages.TwoDeadWheelInputsMessage;
 
 @Config
 public final class TwoDeadWheelLocalizer implements Localizer {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/productionOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/productionOpmode.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name="productionOpmode Meet 1", group="Robot")
+@TeleOp(name="productionOpmode Meet 2", group="Robot")
 public class productionOpmode extends LinearOpMode {
 
     @Override
