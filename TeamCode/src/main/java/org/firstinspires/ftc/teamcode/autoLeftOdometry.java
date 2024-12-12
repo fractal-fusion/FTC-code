a1e56b3017dc16c4dcf694a0502c7d56e3c18bed
fractal-fusion <fractal.fusion@27188@gmail.com>
2024-12-12 17:19:43 -0500

    basic odometry auto implemented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoLeftOdometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoLeftOdometry.java
@@ -1,5 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -8,13 +15,41 @@ public class autoLeftOdometry extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Drivetrain drivetrain = new Drivetrain(this);
+        double tilelength = 24;
         Intake intake = new Intake(this);
         Arm arm = new Arm(this);
 
+        //initialize beginning pose and mecanum drive object
+        Pose2d initialPose = new Pose2d(-tilelength, -65, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        //pre-build trajectories during initialization
+        Action gotoBucket = drive.actionBuilder(initialPose)
+                .splineTo(new Vector2d(-52.5,-52.5), Math.toRadians(225))
+                .build();
+
+        Action gotoRightSample = drive.actionBuilder(initialPose)
+                //face sample
+                .turnTo(Math.toRadians(90))
+                //move to rightmost sample
+                .strafeTo(new Vector2d(-48.2,-36.5))
+                .build();
+
+        //pickup a sample
+        Actions.runBlocking(
+                new SequentialAction(
+                        intake.closeClaw(),
+                        arm.moveArmToCollectionDegrees()
+                ));
+
         waitForStart();
 
-        //do something
+        Actions.runBlocking(
+                new SequentialAction(
+                        gotoBucket,
+                        arm.extendViperslides(),
+                        intake.openClaw()
+                ));
 
     }
 }
